93a707c4613ddc32fe499f6a4d9e4a52c43db284
SoKinPui <sosokinpui@gmail.com>
2023-07-06 13:34:22 +0800

    remove markdown from tree sitter

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -12,14 +12,14 @@ require'nvim-treesitter.configs'.setup {
       "c",
       "java",
       "julia",
-      "markdown",
-      "markdown_inline",
+      --"markdown",
+      --"markdown_inline",
       "bash",
   },
   -- 启用代码高亮功能
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = true
+    additional_vim_regex_highlighting = false
   },
   -- 启用增量选择
   incremental_selection = {
@@ -33,7 +33,7 @@ require'nvim-treesitter.configs'.setup {
   },
   -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
   indent = {
-    enable = true
+    enable = false
   }
 }
 -- 开启 Folding

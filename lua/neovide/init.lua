539112c3ca8fdd93161ad3d54619882a0609f6d2
SoKinPui <sosokinpui@gmail.com>
2023-09-26 10:45:09 +0800

    remove unused lua code

--- lua/neovide/init.lua
+++
@@ -1,6 +0,0 @@
-vim.g.neovide_cursor_animation_length = 0.10
-vim.g.neovide_cursor_trail_size = 0.8
-vim.g.neovide_cursor_animate_in_insert_mode = true
-vim.g.neovide_cursor_animate_command_line = true
-
-

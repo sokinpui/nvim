325d0582f506e218e26c86d77a4925aa7d0d8441
SoKinPui <sosokinpui@gmail.com>
2023-10-09 10:18:36 +0800

    fz and fl for FzfLua and resume respectively

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,69 +1,4 @@
 return {
-  --{
-  --  'junegunn/fzf.vim',
-  --  dependencies = {
-  --    { "junegunn/fzf", build = "./install --bin" }
-  --  },
-  --  keys = {
-  --    {"<leader>f"},
-  --    {"<C-p>"},
-  --    {"<C-f>"},
-  --    {"<C-l>"},
-  --  },
-  --  cmd = {
-  --    "Files",
-  --    "GFiles",
-  --    "Buffers",
-  --    "Colors",
-  --    "Ag",
-  --    "Rg",
-  --    "RG",
-  --    "Lines",
-  --    "BLines",
-  --    "Tags",
-  --    "BTags",
-  --    "Marks",
-  --    "Jumps",
-  --    "Windows",
-  --    "Locate",
-  --    "History",
-  --    "Snippets",
-  --    "Commits",
-  --    "BCommits",
-  --    "Commands",
-  --    "Maps",
-  --    "Helptags",
-  --    "Filetypes",
-  --  },
-  --  build = "./install --all",
-  --  config = function()
-  --    vim.cmd([[
-  --    let g:fzf_preview_window = [ 'right,55%', 'ctrl-/' ]
-  --    let g:fzf_layout         = {'window': { 'width': 0.95, 'height': 1 }}
-  --    let g:fzf_buffers_jump   = 1
-  --    let g:fzf_history_dir = '~/.local/share/fzf-history'
-  --
-  --    nnoremap <C-f> <Cmd>Files<cr>
-  --    "nnoremap <leader>ff <Cmd>execute 'Files' system('git rev-parse --show-toplevel 2> /dev/null')[:-2]<cr>
-  --    nnoremap <leader>ff <Cmd>GFiles<Cr>
-  --
-  --    nnoremap <C-l> <Cmd>Lines<cr>
-  --
-  --    nnoremap <C-p> <Cmd>Rg<cr>
-  --
-  --    nnoremap <leader>fo <Cmd>History<cr>
-  --
-  --    nnoremap <leader>fm <Cmd>Marks<cr>
-  --
-  --    nnoremap <leader>fh <Cmd>Helptags<cr>
-  --    nnoremap <leader>fc <Cmd>Commands<cr>
-  --
-  --    "insert relative path of file in project
-  --    " inoremap <expr> <C-l> fzf#vim#complete#path('fd --type=file . $(git rev-parse --show-cdup 2> /dev/null)')
-  --    " inoremap <expr> <C-f> fzf#vim#complete#path('fd --type=file . $(git rev-parse --show-toplevel 2> /dev/null)')
-  --    ]])
-  --  end,
-  --},
   {
     "ibhagwan/fzf-lua",
     -- optional for icon support
@@ -75,7 +10,6 @@ return {
       {"<C-p>"},
       {"<C-g>"},
       {"<C-l>"},
-      {"<leader><C-f>"},
       {"<leader>f"},
     },
     cmd = "FzfLua",
@@ -83,6 +17,8 @@ return {
       local actions = require "fzf-lua.actions"
       -- calling `setup` is optional for customization
       require("fzf-lua").setup({
+        global_resume = true,
+        global_resume_query = true,
         global_file_icons    = false,
         winopts = {
           height = 1,
@@ -142,6 +78,9 @@ return {
       vim.keymap.set("n", "<leader>fo", function () fzf.oldfiles() end, opts)
       vim.keymap.set("n", "<leader>ff", function () fzf.git_files() end, opts)
       vim.keymap.set("n", "<leader>fc", function () fzf.commands() end, opts)
+
+      vim.keymap.set("n", "<leader>fl", function () fzf.resume() end, opts)
+      vim.keymap.set("n", "<leader>fz", function () fzf.builtin() end, opts)
 
       --vim.keymap.set("t", "<C-v>", "<C-\\><C-n>\"+pA", opts)
     end

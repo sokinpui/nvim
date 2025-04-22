10ca20aa821915fdd225a14f17575e96d8c4e9fa
sokinpui <sosokinpui@gmail.com>
2025-04-22 14:42:09 +0800

    add outline

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,6 +1,9 @@
 return {
   {
     "hedyhli/outline.nvim",
+    keys = {
+      { "<leader>ol", "<cmd>Outline<cr>", desc = "Outline" },
+    },
     cmd = { "Outline", "OutlineOpen" },
     config = function()
       require("outline").setup()
